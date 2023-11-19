ed40d1f49b435fa56a385fd266179523798b4608
kledson <pamthercode@gmail.com>
2023-11-19 19:23:23 -0300

    sem palavras

---
+++ novo/index.js
@@ -0,0 +1,38 @@
+const body = document.body
+let lastScroll = 0
+const header = document.querySelector('header')
+window.addEventListener('scroll', ()=>
+{
+    const currentScroll = window.scrollY
+
+    if(currentScroll > lastScroll)
+    {
+        header.classList.remove('sty')
+    }
+    if(currentScroll == lastScroll)
+    {
+        header.classList.add('sty')
+    }
+    
+    
+})
+
+
+
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header navegacao a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
