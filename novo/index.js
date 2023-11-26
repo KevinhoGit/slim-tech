9256e25a2f7ec48aeb84cbd98983e8b8ed67e6d5
kledson <pamthercode@gmail.com>
2023-11-26 11:56:37 -0300

    sem palavras3

--- novo/index.js
+++ novo/index.js
@@ -22,10 +22,13 @@ let navegacao = document.querySelector('.navegacao')
 
 menuicone.onclick = () =>
 {
-    menuicone.classList.toggle('fa-duotone')
+    menuicone.classList.toggle('fa-bars-sort')
     navegacao.classList.toggle('ativa')
 }
-
+navegacao.onclick=()=>
+{
+    navegacao.classList.toggle('ativa')
+}
 
 let sections = document.querySelectorAll('section')
 let links = document.querySelectorAll('header navegacao a')
@@ -34,4 +37,28 @@ let links = document.querySelectorAll('header navegacao a')
 
 
 
+const section = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
 
+
+
+
+const sla = document.querySelectorAll('a');
+sla.onclick(()=>
+{
+    sla.classList.remove('ativa')
+})
